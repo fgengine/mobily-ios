16534685a4a275b31494e2294edb78a2a09950ba
Trifonov Alexander <fgengine@gmail.com>
2015-03-23 08:54:05 +0300

    Add Watch target

---
+++ Demo/Demo WatchKit Extension/GlanceController.h
@@ -0,0 +1,14 @@
+//
+//  GlanceController.h
+//  Demo WatchKit Extension
+//
+//  Created by Alexander Trifonov on 20.03.15.
+//  Copyright (c) 2015 fgengine. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface GlanceController : WKInterfaceController
+
+@end
