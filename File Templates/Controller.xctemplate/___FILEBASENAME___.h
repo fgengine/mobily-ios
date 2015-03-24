862e6dbfb7a102d6da8597a3dda1e977b651eec9
zozo4kin <zzakharka@gmail.com>
2015-03-24 23:07:33 +0300

    controller templates

---
+++ File Templates/Controller.xctemplate/___FILEBASENAME___.h
@@ -0,0 +1,11 @@
+/*--------------------------------------------------*/
+
+#import "___VARIABLE_baseClass:identifier___.h"
+
+/*--------------------------------------------------*/
+
+@interface ___FILEBASENAMEASIDENTIFIER___ : ___VARIABLE_baseClass:identifier___
+
+@end
+
+/*--------------------------------------------------*/
---
+++ File Templates/Controller.xctemplate/___FILEBASENAME___.m
@@ -0,0 +1,35 @@
+/*--------------------------------------------------*/
+
+#import "___FILEBASENAME___.h"
+
+/*--------------------------------------------------*/
+
+@interface ___FILEBASENAMEASIDENTIFIER___ ()
+
+@end
+
+/*--------------------------------------------------*/
+
+@implementation ___FILEBASENAMEASIDENTIFIER___
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+	 
+    // Do any additional setup after loading the view.
+}
+
+- (void)update {
+    [super update];
+    
+    // update screen data
+}
+
+- (void)clear {
+	// clear all data
+	
+	[super clear];
+}
+
+@end
+
+/*--------------------------------------------------*/
